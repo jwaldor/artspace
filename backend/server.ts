3b0328a794cf4b63bb398a4bce51e86a4aef41b6
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-10-21 18:55:39 -0400

    modify middleware to use different clerk functions

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { Request } from "express";
 import cors from "cors";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, User } from "@prisma/client";
 import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 import { DBService } from "./services/databaseservices";
+import { clerkMiddleware, getAuth, requireAuth } from "@clerk/express";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -10,41 +11,52 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.use(clerkMiddleware());
 
 const dbService = DBService();
 
+interface UserRequest extends Request {
+  user: User;
+}
+
 const authAndUserMiddleware = [
-  ClerkExpressRequireAuth(),
+  requireAuth(),
   async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
   ) => {
-    const clerkUser = req.auth.userId;
-    const user = await dbService.userExists(clerkUser);
-    if (!user) {
-      const newUser = await dbService.createUser(clerkUser);
-      console.log(newUser);
-      req.body.createdById = newUser.id;
+    const auth = getAuth(req);
+    const clerkUser = auth.userId;
+    if (!clerkUser) {
+      res.status(401).json({ error: "Unauthorized" });
+      return;
     }
+    const user: User | null = await dbService.getUser(clerkUser);
+    req.user = user || (await dbService.createUser(clerkUser));
     // console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next();
   },
 ];
 // Define a new middleware function
 
-app.post("/createPost", authAndUserMiddleware, async (req, res) => {
-  const { name, parameters, form, createdById } = req.body;
-  const post = await prisma.artPiece.create({
-    data: {
-      name,
-      parameters,
-      form,
-      createdById,
-    },
-  });
-  res.json(post);
-});
+app.post(
+  "/createPost",
+  authAndUserMiddleware,
+  async (req: UserRequest, res) => {
+    const { name, parameters, form, createdById } = req.body;
+    const user = req.user;
+    const post = await prisma.artPiece.create({
+      data: {
+        name,
+        parameters,
+        form,
+        createdBy: { connect: { id: user.id } },
+      },
+    });
+    res.json(post);
+  }
+);
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
--- backend/services/databaseservices.ts
+++ backend/services/databaseservices.ts
@@ -3,9 +3,9 @@ import { PrismaClient } from "@prisma/client";
 export const DBService = () => {
   const prisma = new PrismaClient();
   return {
-    userExists: async (id: string) => {
+    getUser: async (id: string) => {
       const user = await prisma.user.findUnique({ where: { id } });
-      return user !== null;
+      return user;
     },
     createUser: async (id: string) => {
       const user = await prisma.user.create({ data: { id } });
