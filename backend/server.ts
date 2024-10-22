af7d51fd23995288b8e4685d1e4274366da92283
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-10-22 10:03:31 -0400

    add frontend URL to cors

--- backend/server.ts
+++ backend/server.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(clerkMiddleware());
 
 const dbService = DBService();
