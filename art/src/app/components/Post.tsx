d1151539bb9f762d2a35d7a8af14e252929937c5
jwaldor <98110682+jwaldor@users.noreply.github.com>
2024-10-25 18:15:09 -0400

    add fill color when liked

--- art/src/app/components/Post.tsx
+++ art/src/app/components/Post.tsx
@@ -2,9 +2,9 @@ import { Shiba } from "./artforms/Shiba";
 import { Conway } from "./artforms/Conway";
 import { ArtForm, PostType } from "@/services/artService";
 import { zapiClient } from "@/services/artClient";
-import { useAuth } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { GlobalContext } from "../layout";
-import { useContext } from "react";
+import { useCallback, useContext } from "react";
 
 export function getArtFormComponent(artForm: ArtForm) {
     console.log("artForm", artForm);
@@ -24,6 +24,10 @@ function Post({ post }: { post: PostType }) {
     console.log("post", post);
     const { setPosts } = useContext(GlobalContext);
     const { getToken } = useAuth();
+    const user = useUser();
+    const isLiked = useCallback(() => {
+        return post.likes.find((like) => like.id === user?.user?.id);
+    }, [post.likes, user?.user?.id]);
 
     return (
         <div className="">
@@ -44,7 +48,7 @@ function Post({ post }: { post: PostType }) {
                         const token = await getToken();
                         zapiClient.post("/toggleLikePost", { postId: post.id }, { headers: { Authorization: `Bearer ${token}` } }).then((res) => { if (res.success) zapiClient.get("/allPosts").then((res) => { setPosts(res); console.log("res", res) }) })
                     }}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block" viewBox="0 0 20 20" fill="none" stroke="black" style={{ transform: 'translateY(-2px) translateX(1px)' }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 inline-block" viewBox="0 0 20 20" fill={isLiked() ? "red" : "none"} stroke="black" style={{ transform: 'translateY(-2px) translateX(1px)' }}>
                             <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 0 010-5.656z" clipRule="evenodd" />
                         </svg>
                     </button> <span className="mr-[1px]">{post.likes.length} </span>
